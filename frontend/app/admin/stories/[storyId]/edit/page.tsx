c8938684094e295a993841a8bf174990e17df17f
shivam jaiswal <shivam.jaiswal2211@gmail.com>
2026-01-02 12:23:16 +0530

    force edit page to be valid module

--- frontend/app/admin/stories/[storyId]/edit/page.tsx
+++ frontend/app/admin/stories/[storyId]/edit/page.tsx
@@ -1,13 +1,3 @@
-// frontend/app/admin/stories/[storyId]/edit/page.tsx
-"use client";
-
-import React from "react";
-
-export default function AdminStoryEditPage() {
-  return (
-    <div style={{ padding: 16 }}>
-      <h1>Edit Story</h1>
-      <p>Page is loading correctly. Now add your edit UI here.</p>
-    </div>
-  );
+export default function Page() {
+  return null;
 }
